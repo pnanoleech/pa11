b66e073664286f4612d61ff802b22f7e0eec4f8e
pnanoleech <001upset@gmail.com>
2024-06-04 10:34:59 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -220,20 +220,20 @@ function s_validate() {
 </script>
 </head>
 <body>
-  <!--© Designed and coded by @bipuldey19-Telegram-->
+  <!--© Edited by @tusharbyas -Telegram-->
     <header>
       <div class="brand">
         <img
           src="https://graph.org/file/1a6ad157f55bc42b548df.png"
           alt="logo"
         />
-        <a href="https://t.me/krn_adhikari">
+        <a href="https://t.me/botsnano">
           <h2 class="name">Bittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/weebzone/WZML"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/krn_adhikari"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/botsnano"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/nano_leech"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <div id="sticks">
@@ -612,20 +612,20 @@ section span{
     </style>
   </head>
 <body>
-   <!--© Designed and coded by @bipuldey19-Telegram-->
+   <!--© Edited by @tusharbyas -Telegram-->
     <header>
       <div class="brand">
         <img
           src="https://graph.org/file/1a6ad157f55bc42b548df.png"
           alt="logo"
         />
-        <a href="https://t.me/krn_adhikari">
+        <a href="https://t.me/botsnano">
           <h2 class="name">Bittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/weebzone/WZML"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/krn_adhikari"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/botsnano"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/nano_leech"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -780,7 +780,7 @@ def set_priority(id_):
 
 @app.route('/')
 def homepage():
-    return "<h1>See WZML-X <a href='https://www.github.com/weebzone/WZML'>@GitHub</a> By <a href='https://github.com/weebzone'>Code With Weeb</a></h1>"
+    return "<h1>See WZML-X <a href='https://t.me/botsnano'>@GitHub</a> By <a href='https://github.com/weebzone'>Code With Weeb</a></h1>"
 
 
 @app.errorhandler(Exception)
